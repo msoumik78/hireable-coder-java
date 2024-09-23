64ab41ab7348173d319b9cd334a57366053907f3
Soumik Mukherjee <msoumik78@gmail.com>
2024-09-23 12:28:22 +0530

    working code

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,22 +1,15 @@
 package com.example.demo;
 
-
-import java.util.concurrent.FutureTask;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class DemoApplication {
-  public static void main(String[] args) throws Exception{
-    System.out.println("Running in:"+Thread.currentThread().getName());
-    Thread thread1 = new Thread(new RunnableTask());
-    Thread thread2 = new Thread(new RunnableTask());
-    thread1.start();
-    thread2.start();
-
-
-    CallableTask callableTask = new CallableTask();
-    FutureTask<String> ft = new FutureTask<>(callableTask);
-    Thread thread3 = new Thread(ft);
-    thread3.start();
-    System.out.println(ft.get());
+  public static void main(String[] args) {
+    ExecutorService executorService = Executors.newFixedThreadPool(2);
+    for (int i=0; i < 6; i++) {
+      executorService.submit(new RunnableTask());
+    }
+    executorService.isShutdown();
 
   }
 }
