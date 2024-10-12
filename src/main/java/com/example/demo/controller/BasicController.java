a7cb407f58b7b8f365365f44e88ada5b28355487
Soumik Mukherjee <msoumik78@gmail.com>
2024-10-12 19:10:36 +0530

    working version

--- src/main/java/com/example/demo/controller/BasicController.java
+++ src/main/java/com/example/demo/controller/BasicController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import com.example.demo.config.UserListConfig;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,14 +12,8 @@ public class BasicController {
   @Value("${spring.application.name}")
   private String sampleProperty;
 
-  private final UserListConfig userListConfig;
-
   @RequestMapping("/config")
   String getConfig() {
-    System.out.println(userListConfig.getLiveUsers().size());
-    userListConfig.getLiveUsers().forEach((k,v) -> {
-      System.out.println("key :"+k+", value :"+v);
-    });
     return this.sampleProperty;
   }
 }
