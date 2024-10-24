f0ffa945699cbf8c9cdc1b684ec1e7ca9b267ce8
Soumik Mukherjee <msoumik78@gmail.com>
2024-10-24 13:23:20 +0530

    working version

--- src/main/java/com/example/demo/CreateCredentials.java
+++ src/main/java/com/example/demo/CreateCredentials.java
@@ -6,7 +6,7 @@ public class CreateCredentials {
   public static void main(String[] args) {
     String user = "test";
     // replace with password
-    String password = "password";
+    String password = "password123";
     String encoding = Base64.getEncoder().encodeToString((user + ":" + password).getBytes());
     String authHeader = "Basic " + encoding;
     System.out.println(authHeader);
